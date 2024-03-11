c9cbfbce7ababa73345e9492a93fea9f6830c805
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-03-12 01:01:57 +0900

    상품 목록 - 타임 리프

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,43 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    //@Autowired
+    //public BasicItemController(ItemRepository itemRepository){
+    //    this.itemRepository = itemRepository;
+    //위와 같이 final이 붙은 빈을 의존 관계 주입 받으면 @RequiredArgsContructor 사용해주면 자동으로 Autowired 해줌
+    @GetMapping
+    public String items(Model model) {
+       List<Item> items = itemRepository.findAll();
+       model.addAttribute("items", items);
+       return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+
+    }
+}
+
